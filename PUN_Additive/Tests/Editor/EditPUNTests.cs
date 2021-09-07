1bb6f233f9251533cd433c923240f7fba3aebc0d
mrstruijk <mrstruijk@gmail.com>
2021-09-07 15:28:04 +0200

    Removed dependency on Tampering

--- PUN_Additive/Tests/Editor/EditPUNTests.cs
+++ PUN_Additive/Tests/Editor/EditPUNTests.cs
@@ -35,30 +35,7 @@ public class EditPUNTests
 
     public class TamperWith
     {
-        [Test] public void ScaleChangeOnTamper()
-        {
-            var gameObject = new GameObject();
-            var originalScale = gameObject.transform.localScale;
-            var tampering = new Tampering(gameObject.transform);
-            var newScale = new Vector3(3, 3, 3);
-            tampering.TamperScale(newScale);
 
-            Assert.AreNotEqual(originalScale, gameObject.transform.localScale);
-            Assert.AreEqual(newScale, gameObject.transform.localScale);
-        }
-
-
-        [Test] public void PositionChangeOnTamper()
-        {
-            var gameObject = new GameObject();
-            var originalPosition = gameObject.transform.position;
-            var tampering = new Tampering(gameObject.transform);
-            var newPosition = new Vector3(1, 2, 3);
-            tampering.TamperPosition(newPosition);
-
-            Assert.AreNotEqual(originalPosition, gameObject.transform.position);
-            Assert.AreEqual(newPosition, gameObject.transform.position);
-        }
 
     }
 }
